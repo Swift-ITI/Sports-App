df85d635a8039b46289bf4f50a139ff8e223a995
Adham Samer <48602871+AdhamSamer3301@users.noreply.github.com>
2023-02-07 03:08:38 +0200

    Basic Design and Constraints

---
+++ Sports-App/Views/Cells/LeagueDetails/Team/TeamCVCell.swift
@@ -0,0 +1,19 @@
+//
+//  TeamCVCell.swift
+//  Sports-App
+//
+//  Created by Adham Samer on 07/02/2023.
+//
+
+import UIKit
+
+class TeamCVCell: UICollectionViewCell {
+
+    @IBOutlet weak var teamLogo: UIImageView!
+    @IBOutlet weak var teamLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
